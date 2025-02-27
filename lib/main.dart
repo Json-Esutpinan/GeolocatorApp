aec6e34695b9f0905db876aecaf4e0218518c708
Json Estupinan <jeison.code@hotmail.com>
2025-02-27 12:59:39 -0500

    Json: Route update

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:geolocator_bar_app/screens/HomePage.dart';
 import 'package:geolocator_bar_app/screens/login_page.dart';
 import 'package:geolocator_bar_app/screens/RegisterPage.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load();
   runApp(MyApp());
 }
 
